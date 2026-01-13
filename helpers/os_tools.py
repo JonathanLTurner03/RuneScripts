cce26071629d2e23ed72271b1c93f6934154be81
Jonathan Turner <turner@atlantissrv.com>
2026-01-13 18:31:27 -0500

    Testing os_tools

---
+++ helpers/os_tools.py
@@ -0,0 +1,12 @@
+import os
+
+# Checks if the current user is root
+def check_root() -> bool:
+    if os.getuid() == 0:
+        return True
+    return False
+
+# Attempts to elevate privileges using sudo
+def elevate_privileges() -> None:
+    if not check_root():
+        os.execvp("sudo", ["sudo"] + ["python3"] + os.sys.argv)
---
+++ update-scripts.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import os
+from helpers.os_tools import *
+
+elevate_privileges()
